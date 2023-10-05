0ac1edefbda6a4e1806e3b526f781a8d1a9f5381
ahmadfrazz <ahmedfaraz12580@gmail.com>
2023-10-05 16:52:32 +0500

    Invoice dashboard + create + edit + delete

--- src/MainPage/HR/Sales/invoice.jsx
+++ src/MainPage/HR/Sales/invoice.jsx
@@ -3,13 +3,15 @@ import React, { useState,useEffect } from 'react';
 import { Helmet } from "react-helmet";
 import { Link, useNavigate } from 'react-router-dom';
 
-import { DatePicker, Empty, Select, Table, Form, Pagination } from 'antd';
+import { DatePicker, Empty, Select, Table, Form, Pagination, Button, message, Spin } from 'antd';
 import 'antd/dist/antd.css';
 import {itemRender,onShowSizeChange} from "../../paginationfunction"
 import "../../antdstyle.css"
 import { useSelector } from 'react-redux';
 import { LoadingOutlined } from '@ant-design/icons';
 import EmptyTable from "../../../files/Icons/EmptyTable.svg";
+import { apiServices } from '../../../Services/apiServices';
+import Modal from "@mui/material/Modal";
 
 const Invoices = () => {
   
@@ -24,27 +26,45 @@ const Invoices = () => {
   const role = user_state?.user?.role
 
   const [allInvoices, setAllInvoices] = useState([]);
-  const [tableLoader, setTableLoader] = useState(true)
+  const [tableLoader, setTableLoader] = useState(true);
+  const [deleteLoader, setDeleteLoader] = useState(false);
   const [loader, setLoader] = useState(false)
   const [pageSize, setPageSize] = useState(20);
   const [currentPage, setCurrentPage] = useState(1);
   const [paginationDetail, setPaginationDetail] = useState();
   const [filterValues, setFilterValues] = useState();
-
-
-  let d = [
-    {id:1,invoicenumber:"INV-0001",client:"	Global Technologies",createddate:"11 Mar 2019",duedate:"11 Mar 2019",amount:"2099",status:"Paid"},
-    {id:2,invoicenumber:"INV-0002",client:"Delta Infotech",createddate:"11 Mar 2019",duedate:"11 Mar 2019",amount:"2099",status:"Sent"},
-  ]
+  const [fromInvoiceDate, setFromInvoiceDate] = useState('');
+  const [open, setOpen] = useState({
+    isDelOpen: false,
+    data: ''
+  });
 
   useEffect(() => {
     getAllInvoices();
   }, [])
 
-  const getAllInvoices = () => {
-    setAllInvoices(d)
-    setPaginationDetail({total: d.length})
-    setTableLoader(false)
+  const getAllInvoices = (values, current_page, page_size) => {
+    setTableLoader(true);
+    apiServices("GET", `invoices?${values === '' ? '' : values?.fromDate === '' ? '' : values?.fromDate ? `invoiceFrom=${values?.fromDate}` : filterValues?.fromDate ? `invoiceFrom=${filterValues?.fromDate}` : ''}${values === '' ? '' : values?.toDate === '' ? '' : values?.toDate ? `&invoiceTo=${values?.toDate}` : filterValues?.toDate ? `&invoiceTo=${filterValues?.toDate}` : ''}${values === '' ? '' : values?.status === '' ? '' : values?.status ? `&status=${values?.status}` : filterValues?.status ? `&status=${filterValues?.status}` : ''}&page=${current_page ? current_page : currentPage ? currentPage : 1}&limit=${page_size ? page_size : pageSize ? pageSize : 20}`, null, user_state)
+      .then((res) => {
+        if (res?.data?.success === true) {
+          setAllInvoices(res?.data?.Invoices?.docs);
+          setPaginationDetail(res?.data?.Invoices)
+          setTableLoader(false);
+        }
+      })
+      .catch((err) => {
+        setTableLoader(false);
+        message.error(
+          `${
+            err?.response?.data?.msg
+              ? err?.response?.data?.msg
+              : err?.response?.data?.validation?.body?.message
+              ? err?.response?.data?.validation?.body?.message
+              : "Get All Invoices Error"
+          }!`
+        );
+      });
   }
   
   const onFilterFinish = (values) => {
@@ -56,48 +76,115 @@ const Invoices = () => {
     if(formatted_data?.fromDate || formatted_data?.status){
       getAllInvoices(formatted_data, currentPage, pageSize);
       setFilterValues(formatted_data)
-      console.log(formatted_data);
+      // console.log(formatted_data);
     }
-  }  
+  }
+
+  const onHandleDelete = (id) => {
+    setDeleteLoader(true);
+    apiServices("DELETE", "invoices", id, user_state)
+      .then((res) => {
+        if (res?.data?.success === true) {
+          getAllInvoices(filterValues,currentPage, pageSize);
+          handleClose();
+          message.success("Invoice Deleted Successfully!");
+          setDeleteLoader(false);
+        }
+      })
+      .catch((err) => {
+        setDeleteLoader(false);
+        // console.log(err);
+        message.error(
+          `${
+            err?.response?.data?.msg
+              ? err?.response?.data?.msg
+              : err?.response?.data?.validation?.body?.message
+              ? err?.response?.data?.validation?.body?.message
+              : "Delete Invoice Error"
+          }`
+        );
+      });
+  };
+
+  const formatDate = (inputDate) => {
+    if(inputDate){
+      const date = new Date(inputDate);
+      const day = date.getDate();
+      const month = date.toLocaleString('default', { month: 'short' });
+      const year = date.getFullYear();
+  
+      // let daySuffix = "th";
+      // if (day === 1 || day === 21 || day === 31) {
+      //     daySuffix = "st";
+      // } else if (day === 2 || day === 22) {
+      //     daySuffix = "nd";
+      // } else if (day === 3 || day === 23) {
+      //     daySuffix = "rd";
+      // }
+  
+      // const formattedDate = `${day}${daySuffix} ${month}, ${year}`;
+      const formattedDate = `${day} ${month} ${year}`;
+      return formattedDate;
+    }
+}
+
+const handleClose = () => {
+  setOpen({
+    isDelOpen: false,
+    data: ''
+  });
+};
   
     const columns = [
       
       {
         title: '#',
-        dataIndex: 'id',
+        dataIndex: '',
+        render: (text, record, index) => (currentPage - 1) * pageSize + index + 1,
       },      
       {
         title: 'Invoice Number',
-        dataIndex: 'invoicenumber',
+        dataIndex: 'invoiceNo',
         render: (text, record) => (
-          <Link to="/app/sales/invoices-view">#{text}</Link>
+          <Link to="/invoices/view-invoice" state={{invoice_data: record}} style={{color: '#333333'}}>#{text}</Link>
           ),
       },     
       {
         title: 'Client',
-        dataIndex: 'client',
+        dataIndex: 'clientId',
+        render: (text, record) => (
+          <label>{record?.clientId?.clientName}</label>
+          ),
       },
 
       {
         title: 'Created Date',
-        dataIndex: 'createddate',
+        dataIndex: 'invoiceDate',
+        render: (text, record) => (
+          <label>{formatDate(text || '')}</label>
+          ),
       },
       {
         title: 'Due Date',
-        dataIndex: 'duedate',
+        dataIndex: 'dueDate',
+        render: (text, record) => (
+          <label>{formatDate(text || '')}</label>
+          ),
       },    
       {
         title: 'Amount',
-        dataIndex: 'amount',
+        dataIndex: 'totalAmount',
         render: (text, record) => (
-        <span>$ {text}</span>
+          <span>{text?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {record?.currency}</span>
           ),
       },
       {
         title: 'Status',
         dataIndex: 'status',
         render: (text, record) => (
-        <span className={text==="Paid" ? "badge bg-inverse-success" : "badge bg-inverse-info"}>{text}</span>
+        <label className={text==="Paid" ? "badge bg-inverse-success" : text==="Partially Paid" ? "badge bg-inverse-info" : text==="Pending" ? "badge bg-inverse-warning" : text==="Cancelled" ? "badge bg-inverse-danger" : ''}>
+          {text || '-'}
+        </label>
           ),
       },
       {
@@ -106,10 +193,10 @@ const Invoices = () => {
             <div className="dropdown dropdown-action text-end">
                 <a href="#" className="action-icon dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i className="material-icons">more_vert</i></a>
                         <div className="dropdown-menu dropdown-menu-right">
-                          <Link className="dropdown-item" to="/app/sales/invoices-edit"><i className="fa fa-pencil m-r-5" /> Edit</Link>
-                          <Link className="dropdown-item" to="/app/sales/invoices-view"><i className="fa fa-eye m-r-5" /> View</Link>
+                          <Link className="dropdown-item" to="/invoices/edit-invoice" state={{edit_invoice_data: record}}><i className="fa fa-pencil m-r-5" /> Edit</Link>
+                          <Link className="dropdown-item" to="/invoices/view-invoice" state={{invoice_data: record}}><i className="fa fa-eye m-r-5" /> View</Link>
                           <a className="dropdown-item" href="#"><i className="fa fa-file-pdf-o m-r-5" /> Download</a>
-                          <a className="dropdown-item" href="#"><i className="fa fa-trash-o m-r-5" /> Delete</a>
+                          <a className="dropdown-item" href="javascript:void(0)" onClick={() => { setOpen({ isDelOpen: true, data: record }) }}><i className="fa fa-trash-o m-r-5" /> Delete</a>
                         </div>
             </div>
           ),
@@ -163,6 +250,14 @@ const Invoices = () => {
     />
   );
 
+  const handleFromDateChange = (date) => {
+    setFromInvoiceDate(date);
+  };
+  const disabledDate = (current) => {
+    return fromInvoiceDate && current < moment(fromInvoiceDate).endOf('day');
+    // return fromDate && current < moment(fromDate).startOf('day');
+  };
+
 
       return (
         <>
@@ -184,7 +279,7 @@ const Invoices = () => {
               </ul>
             </div>
             <div className="col-auto float-end ms-auto">
-              <Link to="/app/sales/invoices-create" className="btn add-btn"><i className="fa fa-plus" /> Create Invoice</Link>
+              <Link to="/invoices/create-invoice" className="btn add-btn"><i className="fa fa-plus" /> Create Invoice</Link>
             </div>
           </div>
         </div>
@@ -253,6 +348,11 @@ const Invoices = () => {
                     className='form-control filterDate'
                     style={{minHeight: '50px', display: 'flex'}} 
                     getPopupContainer={() => document.getElementById('area')}
+                    onChange={e => {
+                      handleFromDateChange(e);
+                      if(e === null || e){
+                        form.setFieldsValue({ toDate: '' });
+                      }}}
                   />
                 </Form.Item>
             </div>
@@ -281,6 +381,7 @@ const Invoices = () => {
                 className='form-control filterDate'
                 style={{minHeight: '50px', display: 'flex'}} 
                 getPopupContainer={() => document.getElementById('area')}
+                disabledDate={disabledDate}
               />
             </Form.Item>
           </div>
@@ -300,14 +401,22 @@ const Invoices = () => {
                   getPopupContainer={() => document.getElementById('area1')}
                   options={[
                     {
-                        value: 'paid',
-                        label: "Paid",
+                      value: 'Paid',
+                      label: "Paid",
                     },
                     {
-                        value: 'unpaid',
-                        label: "Unpaid",
+                      value: 'Partially Paid',
+                      label: "Partially Paid",
                     },
-                    ]}
+                    {
+                      value: 'Pending',
+                      label: "Pending",
+                    },
+                    {
+                      value: 'Cancelled',
+                      label: "Cancelled",
+                    },
+                  ]}
                 >
                   {/* {
                     allDesignations?.map((item, index) => {
@@ -333,9 +442,10 @@ const Invoices = () => {
               href="javascript:void(0)" type="reset"
               onClick={() => {
                 form.resetFields();
-                // getAllInvoices('', 1, pageSize);
-                // setFilterValues(null);
-                // setCurrentPage(1)
+                getAllInvoices('', 1, pageSize);
+                setFilterValues(null);
+                setCurrentPage(1)
+                setFromInvoiceDate('')
               }}
               className="btn btn-success btn-block w-50 resetButton" style={{backgroundColor: '#616161', color: 'white', borderColor: '#aeaeae'}} 
               // disabled={role === 'admin' ? false : permissions?.viewAllUsers ? false : true}
@@ -358,7 +468,8 @@ const Invoices = () => {
                   // pagination= { {total : allInvoices.length,
                   //   showTotal : (total, range) => `Showing ${range[0]} to ${range[1]} of ${total} entries`,
                   //   showSizeChanger : true,onShowSizeChange: onShowSizeChange ,itemRender : itemRender } }
-                  style = {{overflowX : 'auto'}}
+                  pagination={false}
+                  style = {{overflowX : 'auto', paddingBottom: '130px'}}
                   columns={columns}                 
                   // bordered
                   dataSource={allInvoices}
@@ -380,7 +491,7 @@ const Invoices = () => {
                         onChange={(page, size) => {
                           console.log(page, size);
                           setPageSize(size); setCurrentPage(page);
-                          // getAllClients(filterValues, page, size)
+                          getAllInvoices(filterValues, page, size)
                         }}
                         showSizeChanger={true}
                         pageSizeOptions={['20', '30', '40', '50']}
@@ -394,6 +505,67 @@ const Invoices = () => {
       </div>
       {/* /Page Content */}
     </div>
+
+          {/* delete modall */}
+          <Modal
+        open={open.isDelOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        disableRestoreFocus
+        BackdropProps={{
+          style: { backgroundColor: "rgb(0 0 0 / 87%)" }, // Set the backdrop color here
+        }}
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content" style={{ height: "280px" }}>
+            <div
+              className="modal-body"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <div className="form-header">
+                <h3 style={{ marginBottom: "30px" }}>Delete Invoice</h3>
+                <p>
+                  Are you sure you want to delete <b>{open?.data?.invoiceNo}</b>?
+                </p>
+              </div>
+              <div className="modal-btn delete-action">
+                <div className="row">
+                  <div className="col-6">
+                    <Button
+                      htmlType="submit"
+                      className="btn btn-primary continue-btn"
+                      onClick={() => onHandleDelete(open?.data?._id)}
+                      disabled={deleteLoader}
+                      style={{ width: "100%" }}
+                    >
+                      {deleteLoader ? (
+                        <Spin size="small" indicator={antIcon} />
+                      ) : (
+                        "Delete"
+                      )}
+                    </Button>
+                  </div>
+                  <div className="col-6">
+                    <Button
+                      onClick={handleClose}
+                      className="btn btn-primary submit-btn"
+                      style={{ width: "100%" }}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+
         </>
       
       );
