cdf0b62db2fbf573c8161741a365e1c5fc064c63
ahmadfrazz <ahmedfaraz12580@gmail.com>
2023-10-20 17:31:36 +0500

    task screen completed

--- src/initialpage/Sidebar/sidebar.jsx
+++ src/initialpage/Sidebar/sidebar.jsx
@@ -575,7 +575,12 @@ const Sidebar = (props) => {
                             "active" : pathname.includes('cts-view') ? "active" : ""}
                             to="/projects/project_dashboard">Projects</Link></li>
                       }
-                      <li><Link onClick={() => localStorage.setItem("minheight", "true")} to="/tasks/tasks">Tasks</Link></li>
+                      {/* <li><Link onClick={() => localStorage.setItem("minheight", "true")} to="/tasks/tasks">Tasks</Link></li> */}
+                      {/* <li><Link onClick={() => localStorage.setItem("minheight", "true")} to="/projects/tasks">Tasks</Link></li> */}
+                      { 
+                        (user_state?.role === 'admin' || permissions?.projectManagement) &&
+                        <li><Link onClick={() => localStorage.setItem("minheight", "true")} to="/projects/tasks">Tasks</Link></li>
+                      }
                       <li><Link className={pathname.includes('task-board') ? "active" : ""} to="/app/projects/task-board">Task Board</Link></li>
                     </ul>
                     : ""
