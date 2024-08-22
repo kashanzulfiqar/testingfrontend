91e98ffa1466fe30b5a36ff4a6e4886091abec04
AhmedQaazi <139229770+AhmedQaazi@users.noreply.github.com>
2024-08-22 14:53:07 +0500

    Update allemployees.jsx

--- src/MainPage/Employees/Employees/allemployees.jsx
+++ src/MainPage/Employees/Employees/allemployees.jsx
@@ -872,13 +872,9 @@ const AllEmployees = () => {
               <h5 className="modal-title">
                 Import Excel File
               </h5>
-              <Button
-                type="link"
-                onClick={handleDownloadSample}
-                className="ml-3"
-              >
-                Download Sample
-              </Button>
+              <a href="/sample.xlsx" download="sample.xlsx">
+                Download File
+              </a>      
               <button type="button" className="close" onClick={closeUpload}>
                 <span aria-hidden="true">×</span>
               </button>
