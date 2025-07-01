978a8e3ec713d27470577ccc7f3ad4e44d556238
abdullahbinshahzad <abdullahbinshahzad228@gmail.com>
2025-07-01 11:43:48 +0500

    fix: updating this as in dev to get stripe live.

--- src/router_service/RequireAuth.js
+++ src/router_service/RequireAuth.js
@@ -21,6 +21,7 @@ const RequireAuth = ({Role}) => {
     const location = useLocation();
     const superAdmin = useSelector((state) => state.superAdmin);
     const hasCheckedSubscriptionRef = useRef(false);
+    const permissions = useSelector((state) => state.permissionsSlice.data);
 
     let AuthRole = value ? true : false
 
@@ -146,7 +147,7 @@ const RequireAuth = ({Role}) => {
     }
 
     // Check for non-admin roles with incomplete subscription
-    if (role === '' && companyDetails?.subscriptionStatus === 'incomplete') {
+    if (role === '' && companyDetails?.subscriptionStatus === 'incomplete' && !permissions?.stripeManagement) {
       return (
         <div style={{padding: '20px 0px'}}>
           <div className="account-logo pt-3 pb-2" style={{ textAlign: 'center' }}>
@@ -163,7 +164,7 @@ const RequireAuth = ({Role}) => {
                 <div className="account-wrapper">
                   {/* <h3 className="account-title" style={{ padding: '17px 0px 40px 0px' }}>Subscription Status</h3> */}
                   <div style={{ textAlign: 'center', marginBottom: '30px' }}>
-                    <h2 style={{ color: '#333', marginBottom: '15px' }}>Subscription Cancelled</h2>
+                    <h2 style={{ color: '#333', marginBottom: '15px' }}>Subscription Expired</h2>
                     <p style={{ color: '#666', fontSize: '16px', lineHeight: '1.5' }}>
                       Please contact your administrator to activate the subscription.
                     </p>
@@ -186,7 +187,7 @@ const RequireAuth = ({Role}) => {
     }
 
     // Check for incomplete subscription before any other logic
-    if (role === 'admin' && companyDetails?.subscriptionStatus === 'incomplete') {
+    if ((role === 'admin' || permissions?.stripeManagement) && companyDetails?.subscriptionStatus === 'incomplete') {
       // If we're already on the payment setup page, show it
       if (location.pathname === '/payment/setup') {
         return <PaymentSetup />;
