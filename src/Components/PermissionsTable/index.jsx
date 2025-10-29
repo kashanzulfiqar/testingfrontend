7ae275a9f25a8ee887e2ba294a959610bb1277d2
abdullahbinshahzad <abdullahbinshahzad228@gmail.com>
2025-10-29 12:33:00 +0500

    fix: added assets managment permission column.

--- src/Components/PermissionsTable/index.jsx
+++ src/Components/PermissionsTable/index.jsx
@@ -504,6 +504,24 @@ const PermissionsTable = ({ permissions, setPermissions, disabled }) => {
             </>
         )},
     },
+    {
+      title: 'Assets Management',
+      dataIndex: 'assetsManagement',
+      render: (text, record) => {
+        const sub = record?.subPermissions?.find(permission => permission.title === "Assets Management");
+        const isChecked = record?.subPermissions?.find(permission => permission.title === "Assets Management")?.checked;
+        return(
+            <>
+              {
+                record?.subPermissions?.find(permission => permission.title === "Assets Management") ?
+                  <Checkbox checked={isChecked || false} onChange={() => handleSingleCheckbox(record, sub)} disabled={disabled} style={{color: `${isChecked ? '#ff9b44' : '#B8B8B8'}`}} /> :
+                  <Tooltip title="Permission Not Available For This Module">
+                    <span><Checkbox indeterminate={true} disabled /></span>
+                  </Tooltip>
+              }
+            </>
+        )},
+    },
   ]
 
   return (
